4e94063e156e22b14d5a4901f24c4e9968d28d79
Pooria-Sarkhanzade <104061307+pooriagg@users.noreply.github.com>
2023-06-12 19:37:50 +0330

    Update ChatRoom.tsx
    Make code more clear 💎

--- src/components/ChatRoom.tsx
+++ src/components/ChatRoom.tsx
@@ -82,58 +82,59 @@ const ChatRoom: React.FunctionComponent = (): JSX.Element => {
 
     const sendMessage = useCallback(async () => {
         try {
-            if (await checkNetwork!()) {
-                if (checkWallet!()) {
-                    if (message.length >= 0) {
-                        await toast.promise(async () => {
-                            let from, to, data, value, chainId, gas, gasPrice, nonce;
-
-                            from = userAccount;
-                            to = chatHttps.options.address; 
-                            data = chatHttps.methods.sendMessage(recepient, message).encodeABI({ from });
-                            nonce = await web3?.eth.getTransactionCount(userAccount!);
-                            gasPrice = await web3?.eth.getGasPrice();
-                            gas = await web3?.eth.estimateGas({ to, from, data });
-                            value = "0x0";
-                            chainId = 80001;
-
-                            const TX = {
-                                from,
-                                to,
-                                data,
-                                value,
-                                chainId,
-                                nonce,
-                                gas,
-                                gasPrice
-                            };
-
-                            //@ts-ignore
-                            const { status } = await web3?.eth.sendTransaction(TX);
-                            
-                            if (!status) {
-                                console.error("Falied to send message !");
-                            };
-                        }, {
-                            success: "Message sent",
-                            pending: "Sending message ...",
-                            error: "Failed to send message"
-                        });
-                    } else {
-                        toast.warn("Enter valid message !", {
-                            toastId: "Enter valid message"
-                        });
-                    };
-                } else {
-                    toast.warn("Wallet is not connected to the network !", {
-                        toastId: "Wallet is not connected to the network"
-                    });
-                };
-            } else {
-                toast.warn("Please change your chain, to Polygon-Mumbai Testnet.", {
+            if (!(await checkNetwork!())) {
+                return  toast.warn("Please change your chain, to Polygon-Mumbai Testnet.", {
                     toastId: "Incorrect chain detected"
-                });
+                }); 
             };
+            
+            if (checkWallet!()) {
+                return  toast.warn("Wallet is not connected to the network !", {
+                    toastId: "Wallet is not connected to the network"
+                });    
+            };
+            
+            if (!message.length) {
+                return  toast.warn("Enter valid message !", {
+                    toastId: "Enter valid message"
+                });    
+            };
+            
+            // Execute the main code after validations
+            await toast.promise(async () => {
+                let from, to, data, value, chainId, gas, gasPrice, nonce;
+
+                from = userAccount;
+                to = chatHttps.options.address; 
+                data = chatHttps.methods.sendMessage(recepient, message).encodeABI({ from });
+                nonce = await web3?.eth.getTransactionCount(userAccount!);
+                gasPrice = await web3?.eth.getGasPrice();
+                gas = await web3?.eth.estimateGas({ to, from, data });
+                value = "0x0";
+                chainId = 80001;
+
+                const TX = {
+                    from,
+                    to,
+                    data,
+                    value,
+                    chainId,
+                    nonce,
+                    gas,
+                    gasPrice
+                };
+
+                //@ts-ignore
+                const { status } = await web3?.eth.sendTransaction(TX);
+
+                if (!status) {
+                    console.error("Falied to send message !");
+                };
+            }, {
+                success: "Message sent",
+                pending: "Sending message ...",
+                error: "Failed to send message"
+            });
         } catch (error) {
             toast.error("Failed to send message !", {
                 toastId: "Failed to send message"
